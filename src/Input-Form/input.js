b4549a982d653666d5e2ed92096c7f573b621b22
Stephenna J Corry <Sjcstephenna@gmail.com>
2021-03-28 00:49:23 -0400

    Finished

--- src/Input-Form/input.js
+++ src/Input-Form/input.js
@@ -1,71 +1,92 @@
 import React, { Component }  from 'react';
 
+
 const validEmailRegex = 
 RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
 
+const vForm = (errors) => {
+  let valid = true;
+  Object.values(errors).forEach( val => val.length > 0 && (valid = false));
+  return valid;
+}
 class Register extends Component {
 
-    constructor(props){
-        super(props);
+  constructor(props){
+    super(props)
 
-        this.state = {
-            name: null,
-            email: null,
-            password: null,
-            errors: {
-                name: '',
-                email: '',
-                password: '',
-            }
-        };
+    this.state = {
+      name: null,
+      email: null,
+      password: null,
+      errors: {
+        name: '',
+        email: '',
+        password: '',
+      }
+    };
+  }
+  handleChange = (e) => {
+    e.preventDefault();
+    let { name, value } = e.target;
+    let { errors } = this.state;
+
+    switch(name){
+      case 'fname':
+        errors.name = value.length < 5 ? 'Name must be 5 characters in length' : '';
+        break;
+      case 'emailAddy':
+        errors.email = validEmailRegex.test(value) ? '' : 'Email is invalid';
+        break;
+      case 'password':
+        errors.password = value.length < 8 ? 'Password must be 8 characters in length' : '';
+        break;
+        default: 
+        break;
     }
+    this.setState( {errors, [name]: value} )
+  }
 
-    handleChange = (e) => {
-        e.preventDefault();
-        let {name, value} = e.target;
-        let errors = this.state.errors;
-        switch(name){
-            case 'fname':
-               errors.name = value.length < 5 ? "Name needs to be longer than 5 characters" : '';
-                break;
-            case 'emailAddy':
-                errors.email = validEmailRegex.test(value) ? '' : 'Email is invalid';
-                break;
-            case 'password':
-                errors.password = value.length < 8 ? 'Password needs to be longer than 8 characters' : '';
-                break;
-            default:
-                break;
-        }
+ handleSubmit = (e) => {
+   e.preventDefault();
+  let { errors } = this.state;
 
-        this.setState({errors, [name]: value}, () => {
-            console.log(errors)
-        })
-    }
+  if(vForm(errors)){
+    console.info('Valid Form')
+  } else {
+    console.error('Invalid Form')
+  }
+ }
+ 
     
     render(){
+      const {errors} = this.state;
         return(
             <div className="body">
               <div className="form">
-                <h1>Register</h1>
-                <form  noValidate  >
+                <h1>REGISTER</h1>
+                <form  onSubmit={this.handleSubmit} noValidate >
                   <div className="fname">
                     <label htmlFor="names">Full Name:</label>
-                    <input type="text" name="fname" onChange={this.handleChange} noValidate/>
+                    <input type="text" name="fname" placeholder="5 character minimum" onChange={this.handleChange} noValidate/>
+                    {errors.name.length > 0 && <div className='error'>{errors.name}</div>}
                   </div>
                   <div className="email">
                     <label htmlFor="email">Email:</label>
                     <input type="email" name="emailAddy" onChange={this.handleChange} noValidate/>
+                    {errors.email.length > 0 && <div className='error'>{errors.email}</div>}
                   </div>
                   <div className="pass">
                     <label htmlFor="password">Password:</label>
-                    <input type="password" name="password" onChange={this.handleChange} noValidate/>
+                    <input type="password" name="password" placeholder="8 character minimum"onChange={this.handleChange} noValidate/>
+                    {errors.password.length > 0 && <div className='error'>{errors.password}</div>}
                   </div>
-                  <div className="restrictions">
-                    <p>Password must be eight character in length</p>
+                  {/* <div className="info">
+                    <small>Password must be eight character in length</small>
+                  </div> */}
+                  <div className="submit">
+                    <button>CREATE</button>
                   </div>
                 </form>
-                <button>Create</button>
               </div>
               <div className="circle1"></div>
               <div className="circle2"></div>
